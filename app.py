88bf7d8e806cf1988659b6302bd1e60d66adc3c7
RachUwU <maksimvrus371@gmail.com>
2024-11-26 15:30:20 +0300

    changes for pagination

--- app.py
+++ app.py
@@ -281,8 +281,9 @@ def get_saved_answers_from_database(session_num):
 db = SQLAlchemy(app)
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    isAdmin = db.Column(db.Integer)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    isAdmin = db.Column(db.Boolean, nullable=False)
 
 @app.route('/admin')
 def admin():
